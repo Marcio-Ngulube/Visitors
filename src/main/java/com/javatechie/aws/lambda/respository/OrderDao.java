7e2723f89ee539ce1602993ad76b76f100aeba76
Rost Max <ngulubemarcio@gmail.com>
2022-05-18 15:23:17 +0200

    backend information

---
+++ src/main/java/com/javatechie/aws/lambda/respository/OrderDao.java
@@ -0,0 +1,31 @@
+package com.javatechie.aws.lambda.respository;
+
+import com.javatechie.aws.lambda.domain.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+public class OrderDao {
+
+
+    public List<Order> buildOrders(){
+        return Stream.of(
+                new Order(101, "Mobile", 20000, 1),
+                new Order(102, "Book", 999, 2),
+                new Order(278, "Book", 1466, 3),
+                new Order(953, "Jeans", 4499, 1)
+        ).collect(Collectors.toList());
+    }
+
+    public List<Order> buildOrders(String teste){
+        return Stream.of(
+                new Order(101, "Mobile "+teste, 20000, 1),
+                new Order(102, teste, 999, 2),
+                new Order(278, "Book "+teste, 1466, 3),
+                new Order(953, "Jeans", 4499, 1)
+        ).collect(Collectors.toList());
+    }
+}
