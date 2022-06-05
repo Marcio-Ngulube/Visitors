60e9891c411fd4d25b9c5dafbd17206f3e0e326d
Rost Max <ngulubemarcio@gmail.com>
2022-06-05 23:34:14 +0200

    debugging: tracking the problem with undefined on the beginning

--- js/visitor_view.js
+++ js/visitor_view.js
@@ -24,7 +24,10 @@ function getVisitorNameAndRegNumber(){
 		xhttp.onreadystatechange = function () {
 
 			 if (xhttp.readyState == XMLHttpRequest.DONE) {
-        			const objects = JSON.parse(this.responseText);
+			 	      response_data=this.responseText;
+			 	      console.log(response_data);
+			 	      console.log(visitor_url+"?method=getVisitor&&value="+localStorage["visitor_reg_number"]);
+        			const objects = JSON.parse(response_data);
         			document.getElementById("visitor_name").innerHTML = "Thank you for registering: "+objects['visitor_name'];
         			document.getElementById("visitor_reg_number").innerHTML = "Register Number: "+objects['visitor_register_num'];
     		}
