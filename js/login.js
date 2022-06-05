839b75abca83a1fdca68ac35a403b8ee3fe9569b
Rost Max <ngulubemarcio@gmail.com>
2022-06-05 18:16:31 +0200

    testing: cors problem fixing

--- js/login.js
+++ js/login.js
@@ -1,21 +1,27 @@
 
-
+/*
 var ip='';
 ip = location.host;
 ip=ip.split(':')[0];
 localStorage["ip"]=ip;
 localStorage["ip"]=ip+"localhost:6379";
-console.log("ip "+localStorage["ip"]);
+console.log("ip "+localStorage["ip"]);*/
+var visitor_url ="https://85o0pmpnv9.execute-api.us-east-1.amazonaws.com/default/marnvisitor";
+key="";
+
+
 //localStorage["ip"]="localhost";
 function sayHello() {
 	
-	var  utilizador = document.loginForm.username.value;
-	var  senha= document.loginForm.password.value;
-
-	console.log("ip in: "+ip);
+	var  name = document.loginForm.name.value;
+	var  code= document.loginForm.code.value;
 
 	const xhttp = new XMLHttpRequest();
-		xhttp.open("POST", "http://"+localStorage["ip"]+"/login",true);
+	
+
+   /*
+	const xhttp = new XMLHttpRequest();
+		xhttp.open("GET", "http://"+localStorage["ip"]+"/login",true);
 		xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 		
 		console.log("aguento");
@@ -39,18 +45,74 @@ function sayHello() {
 			}
 		};
 		
-		xhttp.send(JSON.stringify(
-			{
-				"username" : ""+utilizador,
-				"password" : ""+senha
-			}
-		));	
+		*/
 }
 
 function load_page(){
 	
 window.location.href = "principal.html";
 };
+
+function initial(){
+
+	setVisitorNumber();
+	makeid(5);
+}
+
+function setVisitorNumber(){
+
+	const xhttp = new XMLHttpRequest();
+		xhttp.open("GET",visitor_url+"?method=getNumberOfVisits",true);
+		xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+		//xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		
+		console.log("aguento");
+		
+		xhttp.onreadystatechange = function () {
+
+			 if (xhttp.readyState == XMLHttpRequest.DONE) {
+        			alert(xhttp.responseText);
+    		}
+			
+			/*if (xhttp.readyState === 4) {
+				const objects = JSON.parse(this.responseText);
+				console.log("nos ss"+xhttp.status);
+				console.log("devolve "+objects);
+				if (objects['response_code'] === 200) {
+					console.log("devolve "+objects);
+					/*localStorage["data"] = objects['token'];
+					var employees = localStorage["data"];
+					console.log("Sucesso lista de "+employees);
+					alert("Sucess");
+					localStorage["username"]=utilizador;
+					load_page();
+				} else {
+					console.log("Failed "+objects);
+					//alert("Failed "+objects['descricao']);
+				}
+			}*/
+		};
+
+		xhttp.send('');
+
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   key = result;
+
+   document.getElementById("h1").innerHTML = "Key: "+result;
+}
+
+initial();
+
+
 
    
 
